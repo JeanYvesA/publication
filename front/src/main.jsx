67d609c21bd5949a6d9186f59141534f5ee4a295
Jean-Yves AGBESSI <jeagbessi@gmail.com>
2024-10-23 07:56:43 +0100

    Installation React

---
+++ front/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// const elementJsx = <h2>Voici un titre</h2>
+
+// const Application = () => {
+//   return <div>
+//     <h1>Voici un composant</h1>
+//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium blanditiis ipsa obcaecati velit quisquam beatae quia, mollitia accusantium perspiciatis dicta natus alias et reiciendis veritatis. Libero provident voluptatum modi voluptas!</p>
+//   </div>
+  
+// }
+
+class Application extends React.Component{
+  render(){
+    return <div>
+         <h1>Voici un composant en classe</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium blanditiis ipsa obcaecati velit quisquam beatae quia, mollitia accusantium perspiciatis dicta natus alias et reiciendis veritatis. Libero provident voluptatum modi voluptas!</p>
+     </div>
+  }
+}
+
+// ReactDOM.createRoot(document.getElementById("root")).render(elementJsx)
+ReactDOM.createRoot(document.getElementById("root")).render(<Application/>)
